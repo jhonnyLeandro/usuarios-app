893e7b95d5b055bb244076d51b2f505835e9a553
johhnny escobar <ejohhnny@gmail.com>
2020-04-22 14:08:33 -0500

    Add service to get data from rest api

---
+++ src/services/UserService.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  private apiServer = "http://localhost:3000/";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+  constructor(private httpClient: HttpClient) { }
+
+  get(): Observable<any[]> {
+    return this.httpClient.get<any[]>(this.apiServer + 'users');
+  }
+  
+}
