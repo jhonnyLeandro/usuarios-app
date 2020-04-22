192603858887eb02ba0114521b9b66eba0969400
johhnny escobar <ejohhnny@gmail.com>
2020-04-22 14:12:00 -0500

    Add service to get data from api rest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/UserService'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-usuarios';
+
+export class AppComponent implements OnInit {
+  title = 'management users app';
+
+  users = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(){
+    this.userService.get().subscribe((data: any[])=>{
+      this.users = data;
+    })  
+  }
 }
