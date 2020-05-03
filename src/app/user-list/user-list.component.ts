2926de86d46939672c84b99635cf7838a072dcee
jhonny-leandro <ejohhnny@gmail.com>
2020-05-03 16:39:40 -0700

    Add out element to connect with parent componet

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { UserService } from '../../services/UserService'
 
 @Component({
@@ -10,11 +10,19 @@ export class UserListComponent implements OnInit {
 
   users = [];
 
+  @Output() messageEvent = new EventEmitter<boolean>();
+
   constructor(private userService: UserService) { }
 
   ngOnInit(){
     this.userService.get().subscribe((data: any[])=>{
       this.users = data;
+      console.log (this.users);
     })  
   }
+
+  sendMessage() {
+    this.messageEvent.emit(true);
+  }
+
 }
