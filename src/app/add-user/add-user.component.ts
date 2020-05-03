d827273727a59efae712705263ff54bfabb76a10
jhonny-leandro <ejohhnny@gmail.com>
2020-05-03 16:38:01 -0700

    Add form and add outer to connect with parent component

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-add-user',
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class AddUserComponent implements OnInit {
 
   constructor() { }
+  @Output() messageEvent = new EventEmitter<boolean>();
 
   ngOnInit(): void {
+  }
+
+  sendMessage() {
+    this.messageEvent.emit(false);
   }
 
 }
